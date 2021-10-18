7be1aa5edc829c2c3daab7753f608988a61ce79a
Tarek <kheirtarek@hotmail.fr>
2021-10-18 19:08:35 +0200

    fix 100 to 102

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,6 +7,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """random function"""
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,6 +9,7 @@ T = TypeVar("T")
 
 
 def safely_get_value(dct: Mapping[str, str], key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """random function"""
     if key in dct:
         return dct[key]
     else:
