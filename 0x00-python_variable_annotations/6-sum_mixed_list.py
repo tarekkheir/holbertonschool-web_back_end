df0141720dfbe8cbb87ea8d7054412e6a6b4c9a0
Tarek <kheirtarek@hotmail.fr>
2021-10-18 19:07:20 +0200

    fix 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,10 +3,10 @@
 Type-annoted finctions module
 """
 
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_list: Union[int, float]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """return sum of mixed list argument"""
     s: float = 0
     for i in mxd_list:
