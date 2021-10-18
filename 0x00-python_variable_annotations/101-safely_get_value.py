9e1cd304dcabaf006ef425ce65c78f5c5629094f
Tarek <kheirtarek@hotmail.fr>
2021-10-18 19:09:40 +0200

    fix 101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,7 @@ from typing import Mapping, Any, Union, TypeVar
 T = TypeVar("T")
 
 
-def safely_get_value(dct: Mapping[str, str], key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     """random function"""
     if key in dct:
         return dct[key]
