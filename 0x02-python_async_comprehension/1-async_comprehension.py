e449bb65f2c3000b1d7da4659e18b3b406982edc
Tarek <kheirtarek@hotmail.fr>
2021-10-20 15:50:51 +0200

    fix 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension():
     """Get the result of a coroutines function and return it"""
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [i async for i in async_generator()]
