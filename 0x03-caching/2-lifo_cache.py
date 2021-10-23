35a708c429d35c3fce22fd6ab20790a234460806
Tarek <kheirtarek@hotmail.fr>
2021-10-23 21:34:53 +0200

    fix 2

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -15,15 +15,16 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """Add item in the cache"""
         if key and item:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data.keys():
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS
+            and key not in self.cache_data.keys():
                 k = self.history[-1]
                 self.cache_data.pop(k)
                 self.cache_data.update({key: item})
-                self.history.append(key)
                 print("DISCARD: " + k)
             else:
                 self.cache_data.update({key: item})
-                self.history.append(key)
+
+            self.history.append(key)
 
     def get(self, key):
         """Get item from the cache"""
