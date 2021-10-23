0a403207193d92ccbdf9c2d21c9707777fd2f61a
Tarek <kheirtarek@hotmail.fr>
2021-10-23 21:16:22 +0200

    fix 0

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -9,11 +9,13 @@ class BasicCache(BaseCaching):
     """caching system"""
 
     def put(self, key, item):
+        """Add item in the cache"""
         if key and item:
             self.cache_data.update({key: item})
 
     def get(self, key):
-        if key and key in self.cache_data:
+        """Get item from the cache"""
+        if key and key in self.cache_data.keys():
             return self.cache_data[key]
         else:
             return None
