0b6f981e3f7e005c03036e8ae0872c76ec0fb863
Tarek <kheirtarek@hotmail.fr>
2021-10-23 21:30:03 +0200

    fix 2

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -10,26 +10,20 @@ class LIFOCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
-        self.last_key = []
+        self.history = []
 
     def put(self, key, item):
         """Add item in the cache"""
         if key and item:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data.keys():
-                if self.last_key:
-                    k = self.last_key[0]
-                    print("Cache is full")
-                else:
-                    k = list(self.cache_data.keys())[-1]
+                k = self.history[-1]
                 self.cache_data.pop(k)
                 self.cache_data.update({key: item})
-                self.last_key.clear()
-                self.last_key.append(key)
+                self.history.append(key)
                 print("DISCARD: " + k)
             else:
                 self.cache_data.update({key: item})
-                self.last_key.clear()
-                self.last_key.append(key)
+                self.history.append(key)
 
     def get(self, key):
         """Get item from the cache"""
