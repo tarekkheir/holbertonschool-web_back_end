a0868bc76c904e2f682d4b63fe2a4d0a007f9bac
Tarek <kheirtarek@hotmail.fr>
2021-11-04 17:02:02 +0100

    task 2

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> str:
     """GET /api/v1/unauthorized"""
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden"""
+    abort(403)
