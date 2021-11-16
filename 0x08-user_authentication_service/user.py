048f10b47ff3b674fd4fd0a9d8a89cf462643150
Tarek <kheirtarek@hotmail.fr>
2021-11-16 13:13:50 +0100

    task 0

---
+++ 0x08-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+"""SQLAlchemy model"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.expression import false
+from sqlalchemy.sql.schema import ColumnCollectionConstraint
+
+
+Base = declarative_base()
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
