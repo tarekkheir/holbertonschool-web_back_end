c2e60f90cce6b51b7245478f1103076e76668d11
Tarek <kheirtarek@hotmail.fr>
2021-11-17 20:01:11 +0100

    task 11

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,9 +3,10 @@
 
 
 from flask import Flask
-from flask import json
 from flask.globals import request
+from flask.helpers import make_response
 from flask.json import jsonify
+from flask import abort
 from auth import Auth
 
 
@@ -29,6 +30,21 @@ def users():
                         "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """POST /sessions"""
+    email = request.form['email']
+    password = request.form['email']
+
+    if AUTH.valid_login(email, password) is False:
+        abort(401)
+
+    session_id = AUTH.create_session(email, password)
+    res = make_response({"email": email, "message": "logged in"})
+    res.set_cookie("session_id", session_id)
+    return res
 
 
 if __name__ == "__main__":
