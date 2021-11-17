5db18a19d4998921abfff1dd3e98df53f43de32a
Tarek <kheirtarek@hotmail.fr>
2021-11-17 13:44:36 +0100

    fix 1

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,47 +1,35 @@
 #!/usr/bin/env python3
-"""DB module"""
-
-from typing import List
+""" DB module. """
 from sqlalchemy import create_engine
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
+from typing import List
+from sqlalchemy.orm.session import Session
+from typing import Union
+
+from user import Base, User
 
 
 class DB:
-    """DB class
-    """
-
-    args_list = [
-        "id",
-        "email",
-        "hashed_password",
-        "session_id",
-        "reset_token"
-    ]
+    """ DB class. """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """ Initialize a new DB instance """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """ Memorized session object """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add user to the database and return a user object"""
+        """ Add a user to the DataBase and return the user object """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
@@ -49,13 +37,6 @@ class DB:
 
     def find_user_by(self, **args: List[str]) -> User:
         """find user by field in users tables"""
-
-        if args is None:
-            raise InvalidRequestError
-
-        for arg in args:
-            if arg not in self.args_list:
-                raise InvalidRequestError
         try:
             user = self._session.query(User).filter_by(**args).one()
 
