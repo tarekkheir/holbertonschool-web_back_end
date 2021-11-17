112c3b6e57d78950f9d569528902a536d517c16a
Tarek <kheirtarek@hotmail.fr>
2021-11-17 12:52:29 +0100

    task 2

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 """DB module"""
 
+from typing import List
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
     """DB class
     """
+
+    args_list = [
+        "id",
+        "email",
+        "hashed_password",
+        "session_id",
+        "reset_token"
+    ]
 
     def __init__(self) -> None:
         """Initialize a new DB instance
@@ -35,5 +46,22 @@ class DB:
 
         self._session.add(user)
         self._session.commit()
+
+        return user
+
+    def find_user_by(self, **args: List[str]) -> User:
+        """find user by field in users tables"""
+
+        if args is None:
+            raise InvalidRequestError
+
+        for arg in args:
+            if arg not in self.args_list:
+                raise InvalidRequestError
+        try:
+            user = self._session.query(User).filter_by(**args).one()
+
+        except NoResultFound:
+            raise NoResultFound
 
         return user
