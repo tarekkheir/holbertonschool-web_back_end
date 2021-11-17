b6c5c77a9ccc5b17ef78aadcc5904a2d4502be32
Tarek <kheirtarek@hotmail.fr>
2021-11-17 18:30:53 +0100

    task 8

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -32,3 +32,12 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
 
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check if logins are valid"""
+        try:
+            user: User = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(str.encode(password), user.hashed_password)
+
+        except Exception:
+            return False
