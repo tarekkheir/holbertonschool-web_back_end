cf9f012d37643eacea31523832c1042734192cbb
Tarek <kheirtarek@hotmail.fr>
2021-11-23 12:37:27 +0100

    task 1

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,9 @@
 """Unittests"""
 
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,12 +14,22 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
-
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """first test"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), 1)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """second test"""
+        self.assertRaises(KeyError)
+
 
 if __name__ == "__main__":
     unittest.main()
+    # class TestGetJson(unittest.TestCase):
+    #     """class that will test get_json fucntion"""
+    #     @patch()
