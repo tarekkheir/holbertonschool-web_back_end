1b2cae04ba9fb872aa4f9dba1b285375ff294894
Tarek <kheirtarek@hotmail.fr>
2021-11-25 14:02:40 +0100

    task 4

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""i18n module"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """set config for babel object"""
+    LANGUAGES = ["en", "fr"]
+    Babel.default_locale = "en"
+    Babel.default_timezone = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world():
+    """Get method /"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """determine best match with our supported language"""
+    language = request.args.get('locale')
+    if language in app.config['LANGUAGES']:
+        return language
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
