b4b8e71635469095518b85310ca43db1ae3bc06d
Tarek <kheirtarek@hotmail.fr>
2021-11-25 11:13:53 +0100

    task 0

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -8,6 +8,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
