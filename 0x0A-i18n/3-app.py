ba68431378b78c2e769602d30a26a68cb0eeaf78
Tarek <kheirtarek@hotmail.fr>
2021-11-25 12:03:00 +0100

    task 3

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -23,7 +23,7 @@ app.config.from_object(Config)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
     """Get method /"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 @babel.localeselector
