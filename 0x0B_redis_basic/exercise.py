4a8cc0aa964976c981b003a645f1b5bd22b52825
Tarek <kheirtarek@hotmail.fr>
2021-12-02 10:54:17 +0100

    task 4

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -42,12 +42,13 @@ def replay(method: Callable) -> str:
     r = redis.Redis()
     inputs_list = r.lrange(inputs, 0, -1)
     outputs_lists = r.lrange(outputs, 0, -1)
-    l = r.llen(outputs)
+    len_mix = r.llen(outputs)
     mix = zip(inputs_list, outputs_lists)
 
-    print(f"{method.__qualname__} was called {l} times:")
+    print(f"{method.__qualname__} was called {len_mix} times:")
     for k1, k2 in mix:
-        print(f"{method.__qualname__}(*{k1.decode('utf-8')}) -> {k2.decode('utf-8')}")
+        print(f"{method.__qualname__}(*{k1.decode('utf-8')}) ->\
+              {k2.decode('utf-8')}")
 
 
 class Cache():
