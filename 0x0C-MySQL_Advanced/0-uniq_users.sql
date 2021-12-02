b2b98e8e31506246e7c819098e0d593efa443c95
Tarek <kheirtarek@hotmail.fr>
2021-12-02 15:00:41 +0100

    task 0

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- SQL script that creates a table users
-Create table if not exists `holberton`;                                                              
+-- Create users table if not exists                                                              
 Create table if not exists `holberton`.`users` (
-       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       id INT NOT NULL AUTO_INCREMENT,
+       PRIMARY KEY (id),
        email VARCHAR(255) NOT NULL UNIQUE,
        name VARCHAR(255)
 );
