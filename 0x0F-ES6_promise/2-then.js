297338cee3bfb5f903ba6e0c14c836efff540956
Tarek <kheirtarek@hotmail.fr>
2021-12-15 12:31:44 +0100

    task 2

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,7 +1,12 @@
 export default function getResponseFromAPI(promise) {
     promise.then((resolve) => {
         console.log('Got a response from the API');
+        return ({
+            status: 200,
+            body: 'success'
+        })
     }, (reject) => {
         console.log('Got a response from the API');
+        return Error()
     })
 }
