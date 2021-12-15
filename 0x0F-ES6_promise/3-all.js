e2f18869a32a68fbfdbbc57fe0665b2b90f6f868
Tarek <kheirtarek@hotmail.fr>
2021-12-15 14:28:34 +0100

    fix 3

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    return Promise.all([uploadPhoto(), createUser()]).then((values) => {
         const body = values[0]['body']
         const firstName = values[1]['firstName']
         const lastName = values[1]['lastName']
