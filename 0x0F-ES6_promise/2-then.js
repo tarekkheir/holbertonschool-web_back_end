2edf5c54f1455d4d4f8fb58b479562ef903a8d68
Tarek <kheirtarek@hotmail.fr>
2021-12-18 14:42:18 +0100

    fix 2

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function getResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
-    .then(() => console.log('Got a response from the API'));
+    .finally(() => console.log('Got a response from the API'));
 }
