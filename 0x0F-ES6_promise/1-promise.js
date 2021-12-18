ae2ea2f539e84084e72d83a593cef800d80b6094
Tarek <kheirtarek@hotmail.fr>
2021-12-18 14:40:18 +0100

    fix lint 1

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getResponseFromAPI(success) {
-    return new Promise(function (resolve, reject) {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success'
-            });
-        } else {
-            reject(Error('The fake API is not working currently'));
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
