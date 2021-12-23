432c81240cb97843c9fc221197134d1711447c65
Tarek <kheirtarek@hotmail.fr>
2021-12-23 15:16:30 +0100

    task 5

---
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+
+  view[position] = value;
+  return new DataView(buffer);
+}
