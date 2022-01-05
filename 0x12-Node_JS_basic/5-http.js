667500bcc93f26d7bcd23fe96fc1ce33d1f0e72d
Tarek <kheirtarek@hotmail.fr>
2022-01-05 16:38:16 +0100

    task 5

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -9,7 +9,7 @@ const app = http.createServer(async (req, res) => {
   if (req.url === '/' || req.url === '') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.write('This is the list of our students');
+    res.write('This is the list of our students\n');
     countStudents(process.argv[2]).then((data) => {
       res.write(`Number of students: ${data.students.length}\n`);
       res.write(`Number of students in CS: ${data.cs.length}. List: ${data.cs.join(', ')}\n`);
