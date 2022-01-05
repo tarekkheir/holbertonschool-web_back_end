8427524961208c034fd9a3ff03d25988b6654cfd
Tarek <kheirtarek@hotmail.fr>
2022-01-05 13:11:16 +0100

    task 3

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  let data;
+  try {
+    data = await fs.promises.readFile(path, 'utf8');
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+
+  const s = data.split('\n');
+  s.shift();
+
+  const students = s.map((student) => student.split(',')).filter((student) => student.length === 4);
+  const cs = students.filter((student) => student[3] === 'CS').map((student) => student[0]);
+  const swe = students.filter((student) => student[3] === 'SWE').map((student) => student[0]);
+
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+}
+
+module.exports = countStudents;
