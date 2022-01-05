309cdfa5004547f953e841e05d9eb10be764f3bd
Tarek <kheirtarek@hotmail.fr>
2022-01-05 13:40:59 +0100

    task 4

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,13 +1,12 @@
 const http = require('http');
 
 const port = process.env.port || 1245;
+const hostname = 'localhost';
 
 const app = http.createServer(
   (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
     res.end('Hello Holberton School!');
   },
 );
 
-app.listen(port);
+app.listen(port, hostname);
