85954b70cb038f38353e855ed3c7f91b35a1c714
Tarek <kheirtarek@hotmail.fr>
2022-01-05 16:35:12 +0100

    task 5

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -6,19 +6,15 @@ const hostname = 'localhost';
 
 const app = http.createServer(async (req, res) => {
   res.setHeader('Content-type', 'text/plain');
-  switch (req.url) {
-    default:
-      res.end('Hello Holberton School!');
-      break;
-
-    case '/students' || '/students/':
-      res.write('This is the list of our students');
-      countStudents(process.argv[2]).then((data) => {
-        res.write(`Number of students: ${data.students.length}\n`);
-        res.write(`Number of students in CS: ${data.cs.length}. List: ${data.cs.join(', ')}\n`);
-        res.end(`Number of students in SWE: ${data.swe.length}. List: ${data.swe.join(', ')}\n`);
-      }).catch((e) => res.end(e.message));
-      break;
+  if (req.url === '/' || req.url === '') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students');
+    countStudents(process.argv[2]).then((data) => {
+      res.write(`Number of students: ${data.students.length}\n`);
+      res.write(`Number of students in CS: ${data.cs.length}. List: ${data.cs.join(', ')}\n`);
+      res.end(`Number of students in SWE: ${data.swe.length}. List: ${data.swe.join(', ')}\n`);
+    }).catch((e) => res.end(e.message));
   }
 });
 
