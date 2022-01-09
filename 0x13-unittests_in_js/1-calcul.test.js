74362a5612492ebedd4ee8f481548cd8e7b38ea9
Tarek <kheirtarek@hotmail.fr>
2022-01-09 11:42:20 +0100

    fix 1

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -52,38 +52,73 @@ describe('calculateNumber', () => {
   });
 
   describe('DIVIDE', () => {
-    it('DIVIDE: 1.4 / 0', () => {
-      assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    it('Divide by 0', () => {
+      assert.equal(calculateNumber('DIVIDE', 3, 0), 'Error');
     });
-    it('DIVIDE: 4 / 2', () => {
-      assert.equal(calculateNumber('DIVIDE', 4, 2), 2);
+    it('Pos ints pos result: 3 and 2', () => {
+      assert.equal(calculateNumber('DIVIDE', 3, 2), 1.5);
     });
-    it('DIVIDE: 4 / 2.2', () => {
+    it('int and round down: 1 and 3.2', () => {
       assert.equal(calculateNumber('DIVIDE', 4, 2.2), 2);
     });
-    it('DIVIDE: 3 / 2.7', () => {
-      assert.equal(calculateNumber('DIVIDE', 3, 2.7), 1);
+    it('int and round up: 1 and 3.7', () => {
+      assert.equal(calculateNumber('DIVIDE', 6, 2.7), 2);
     });
-    it('DIVIDE: 4.2 / 2', () => {
-      assert.equal(calculateNumber('DIVIDE', 4.2, 2), 2);
+    it('round down and int: 6.3 and 2', () => {
+      assert.equal(calculateNumber('DIVIDE', 6.3, 2), 3);
     });
-    it('DIVIDE: 2.7 / 3', () => {
-      assert.equal(calculateNumber('DIVIDE', 2.7, 3), 1);
+    it('round up and int: 5.6 and 3', () => {
+      assert.equal(calculateNumber('DIVIDE', 5.6, 3), 2);
     });
-    it('DIVIDE: -3.7 / 2', () => {
+    it('neg dividend: -3.7 and 2', () => {
       assert.equal(calculateNumber('DIVIDE', -3.7, 2), -2);
     });
-    it('DIVIDE: 4 / -2.2', () => {
-      assert.equal(calculateNumber('DIVIDE', 4, -2.2), -2);
+    it('neg divisor: 6 and -2.2', () => {
+      assert.equal(calculateNumber('DIVIDE', 6, -2.2), -3);
     });
-    it('DIVIDE: -4 / -2.2', () => {
-      assert.equal(calculateNumber('DIVIDE', -4, -2.2), 2);
+    it('neg dividend and divisor: -6 and -2.2', () => {
+      assert.equal(calculateNumber('DIVIDE', -6, -2.2), 3);
     });
-    it('a not a number', () => {
-      assert.throws(() => calculateNumber('DIVIDE', 'Hello', 3), TypeError);
+    it('a is NaN', () => {
+      assert.throws(() => calculateNumber('DIVIDE', 'Betty', 2), TypeError);
     });
-    it('b not a number', () => {
-      assert.throws(() => calculateNumber('DIVIDE', 3, 'Hello'), TypeError);
+    it('b is NaN', () => {
+      assert.throws(() => calculateNumber('DIVIDE', 1, 'Betty'), TypeError);
     });
+    // describe('DIVIDE', () => {
+    //   it('DIVIDE: 1.4 / 0', () => {
+    //     assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    //   });
+    //   it('DIVIDE: 4 / 2', () => {
+    //     assert.equal(calculateNumber('DIVIDE', 4, 2), 2);
+    //   });
+    //   it('DIVIDE: 4 / 2.2', () => {
+    //     assert.equal(calculateNumber('DIVIDE', 4, 2.2), 2);
+    //   });
+    //   it('DIVIDE: 3 / 2.7', () => {
+    //     assert.equal(calculateNumber('DIVIDE', 3, 2.7), 1);
+    //   });
+    //   it('DIVIDE: 4.2 / 2', () => {
+    //     assert.equal(calculateNumber('DIVIDE', 4.2, 2), 2);
+    //   });
+    //   it('DIVIDE: 2.7 / 3', () => {
+    //     assert.equal(calculateNumber('DIVIDE', 2.7, 3), 1);
+    //   });
+    //   it('DIVIDE: -3.7 / 2', () => {
+    //     assert.equal(calculateNumber('DIVIDE', -3.7, 2), -2);
+    //   });
+    //   it('DIVIDE: 4 / -2.2', () => {
+    //     assert.equal(calculateNumber('DIVIDE', 4, -2.2), -2);
+    //   });
+    //   it('DIVIDE: -4 / -2.2', () => {
+    //     assert.equal(calculateNumber('DIVIDE', -4, -2.2), 2);
+    //   });
+    //   it('a not a number', () => {
+    //     assert.throws(() => calculateNumber('DIVIDE', 'Hello', 3), TypeError);
+    //   });
+    //   it('b not a number', () => {
+    //     assert.throws(() => calculateNumber('DIVIDE', 3, 'Hello'), TypeError);
+    //   });
+    // });
   });
 });
