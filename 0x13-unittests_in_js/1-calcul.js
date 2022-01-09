ef29838dc2faca9e800ed6961d1b7f32cdb6121e
Tarek <kheirtarek@hotmail.fr>
2022-01-09 11:22:54 +0100

    task 1

---
+++ 0x13-unittests_in_js/1-calcul.js
@@ -0,0 +1,28 @@
+function calculateNumber(type, a, b) {
+  const aprime = Math.round(a);
+  const bprime = Math.round(b);
+
+  if (typeof type !== 'string') {
+    throw new TypeError('type must be a string');
+  }
+
+  if (isNaN(aprime) || isNaN(bprime)) {
+    throw new TypeError('a and b must be numbers');
+  }
+
+  if (type === 'SUM') {
+    return (aprime + bprime);
+  } else if (type === 'SUBTRACT') {
+    return (aprime - bprime);
+  } else if (type === 'DIVIDE') {
+
+    if (b === 0) {
+      return ('Error');
+    }
+    return (aprime / bprime);
+  } else {
+    return ('Error')
+  }
+}
+
+module.exports = calculateNumber;
---
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,87 @@
+const calculateNumber = require('./1-calcul');
+const assert = require('assert');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('SUM: 1.4 + 4.5', () => {
+      assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+    it('SUM: 1.7 + 4.5', () => {
+      assert.equal(calculateNumber('SUM', 1.7, 4.5), 7);
+    });
+    it('SUM: -1.4 + 4.5', () => {
+      assert.equal(calculateNumber('SUM', -1.4, 4.5), 4);
+    });
+    it('SUM: 0 + 4.5', () => {
+      assert.equal(calculateNumber('SUM', 0, 4.5), 5);
+    });
+    it('a not a number', () => {
+      assert.throws(() => calculateNumber('SUM', 'Hello', 3), TypeError);
+    });
+
+    it('b not a number', () => {
+      assert.throws(() => calculateNumber('SUM', 3, 'Hello'), TypeError);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('SUBTRACT: 1 - 4', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1, 4), -3)
+    });
+    it('SUBTRACT: 1.4 - 4', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1.4, 4), -3)
+    });
+    it('SUBTRACT: 1.4 - 4.5', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
+    });
+    it('SUBTRACT: 1.7 - 4.5', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
+    });
+    it('SUBTRACT: -1.4 - 4.5', () => {
+      assert.equal(calculateNumber('SUBTRACT', -1.4, 4.5), -6)
+    });
+    it('SUBTRACT: 0 - 4.5', () => {
+      assert.equal(calculateNumber('SUBTRACT', 0, 4.5), -5)
+    });
+    it('a not a number', () => {
+      assert.throws(() => calculateNumber('SUBTRACT', 'Hello', 3), TypeError);
+    });
+    it('b not a number', () => {
+      assert.throws(() => calculateNumber('SUBTRACT', 3, 'Hello'), TypeError);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('DIVIDE: 1.4 / 4.5', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+    it('DIVIDE: 1.4 / 0', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+    it('DIVIDE: 3.6 / 2', () => {
+      assert.equal(calculateNumber('DIVIDE', 3.6, 2), 2);
+    });
+    it('DIVIDE: -3.6 / 2', () => {
+      assert.equal(calculateNumber('DIVIDE', -3.6, 2), -2);
+    });
+    it('DIVIDE: 3.6 / 1.7', () => {
+      assert.equal(calculateNumber('DIVIDE', 3.6, 1.7), 2);
+    });
+    it('DIVIDE: -3.6 / -2', () => {
+      assert.equal(calculateNumber('DIVIDE', -3.6, -2), 2);
+    });
+    it('DIVIDE: -3.6 / -1.2', () => {
+      assert.equal(calculateNumber('DIVIDE', -3.6, -1.2), 4);
+    });
+    it('DIVIDE: 0 / 2', () => {
+      assert.equal(calculateNumber('DIVIDE', 0, 2), 0);
+    });
+    it('a not a number', () => {
+      assert.throws(() => calculateNumber('SUBTRACT', 'Hello', 3), TypeError);
+    });
+    it('b not a number', () => {
+      assert.throws(() => calculateNumber('SUBTRACT', 3, 'Hello'), TypeError);
+    });
+  });
+
+});
