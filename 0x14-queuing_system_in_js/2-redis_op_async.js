27019b0367eef406cb8f1a0a270b2d8066a802e3
Tarek <kheirtarek@hotmail.fr>
2022-01-12 10:51:08 +0100

    task 3

--- 0x14-queuing_system_in_js/2-redis_op_async.js
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -4,27 +4,23 @@ import { promisify } from 'util';
 const client = createClient();
 const getAsync = promisify(client.get).bind(client);
 
-client.connect();
-
 client.on('connect', () => {
-    console.log('Redis client connected to the server');
+  console.log('Redis client connected to the server');
 });
 
 client.on('error', (err) => {
-    console.log(`Redis client not connected to the server: ${err.message}`);
+  console.log(`Redis client not connected to the server: ${err.message}`);
 });
 
 
 function setNewSchool(schoolName, value) {
-    client.set(schoolName, value, print);
+  client.set(schoolName, value, print);
 }
 
 async function displaySchoolValue(schoolName) {
-    console.log(await getAsync(schoolName));
+  await getAsync(schoolName).then((data) => console.log(data));
 }
 
-(async () => {
-    await displaySchoolValue('Holberton');
-    setNewSchool('HolbertonSanFrancisco', '100');
-    await displaySchoolValue('HolbertonSanFrancisco');
-})
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
