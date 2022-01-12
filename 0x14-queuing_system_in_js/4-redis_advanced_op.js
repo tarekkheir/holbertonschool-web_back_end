36e7375b5408ae9ef1ee2ca42c84a1bbcdde1df3
Tarek <kheirtarek@hotmail.fr>
2022-01-12 11:13:21 +0100

    task 4

---
+++ 0x14-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,24 @@
+import { callbackQueue } from 'kue/lib/queue/events';
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`)
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+
+client.hset('HolbertonSchools', 'Portland', 50, print);
+client.hset('HolbertonSchools', 'Seattle', 80, print);
+client.hset('HolbertonSchools', 'New York', 20, print);
+client.hset('HolbertonSchools', 'Bogota', 20, print);
+client.hset('HolbertonSchools', 'Cali', 40, print);
+client.hset('HolbertonSchools', 'Paris', 2, print);
+
+client.hgetall('HolbertonSchools', (err, obj) => {
+  console.log(obj);
+});
